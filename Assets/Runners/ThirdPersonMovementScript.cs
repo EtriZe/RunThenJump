623e0055e32433116b3267705b6463191e077897
EtriZe <valentin.barrere@ynov.com>
2023-06-18 19:03:24 +0200

    Movement + Jump
    Need to remove this screen lag

--- Assets/Runners/ThirdPersonMovementScript.cs
+++ Assets/Runners/ThirdPersonMovementScript.cs
@@ -8,36 +8,56 @@ public class ThirdPersonMovementScript : MonoBehaviour
 {
     #region Private variables
 
-    private float _gravity = -9.81f;
+    [Header("Movement")]
     private float _velocity;
     private float _turnSmoothVelocity;
+    private float _turnSmoothTime = 0.1f;
     private Vector3 _direction;
     private Vector2 _input;
     private Vector3 _moveDir;
 
-    [SerializeField] private float _gravityMultiplier = 5.0f;
+    [Header("Jump")]
     [SerializeField] private float _jumpPower;
+    [SerializeField] private float _jumpCooldown;
+    [SerializeField] private float _airMultiplier;
+    private bool _readyToJump = true;
+
+    [Header("Ground check")]
+    [SerializeField] private float PlayerHeight;
+    [SerializeField] private LayerMask WhatIsGround;
+    [SerializeField] private float GroundDrag;
+    private bool Grounded;
 
     #endregion
     #region Public variables
 
-    public CharacterController Controller;
+    public Rigidbody Rb;
     public Transform Cam;
-    public float Speed = 6.0f;
-    public float TurnSmoothTime = 0.1f;
+    public float Speed;
 
     #endregion
 
+    private void Start() {
+        Rb.freezeRotation = true;
+    }
+
+    void FixedUpdate() {
+        ApplyMovement();
+    }
 
     // Update is called once per frame
     void Update()
     {
         Cursor.lockState = CursorLockMode.Locked;
-
         
+        //Ground check
+        Grounded = Physics.Raycast(transform.position, Vector3.down, PlayerHeight * 0.5f + 0.2f, WhatIsGround);
+
+        if(Grounded) Rb.drag = GroundDrag;
+        else Rb.drag = 0;
+
+        SpeedControl();
         ApplyRotation();
-        ApplyGravity();
-        ApplyMovement();
     }
 
     
@@ -49,46 +69,57 @@ public class ThirdPersonMovementScript : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (!context.started) return;
-        if (!IsGrounded()) return;
+        
+        if(_readyToJump && Grounded)
+        {
+            Rb.velocity = new Vector3(Rb.velocity.x, 0f, Rb.velocity.z);
+            Rb.AddForce(transform.up * _jumpPower, ForceMode.Impulse);
 
-            _velocity += _jumpPower;
-        Debug.Log("Jump");
-
+            Invoke(nameof(ResetJump), _jumpCooldown);
+        }
     }
+
+    private void ResetJump()
+    {
+        _readyToJump = true;
+    }
+    
     private void ApplyRotation()
     {
-        if (_input.sqrMagnitude == 0) return;
+        if (_input.sqrMagnitude == 0) 
+        {
+            _moveDir = Vector3.zero;
+            return;
+        }
 
         //Will rotate the character to the direction
         float targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg + Cam.eulerAngles.y;
 
         //Will smooth the rotation of the character
-        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, TurnSmoothTime);
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
         transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
         //To move in direction of the camera
         _moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;        
     }
 
-    private void ApplyGravity()
-    {
-        if (IsGrounded() && _velocity < 0.0f)
-        {
-            _velocity = -1.0f;
-        }
-        else
-        {
-            _velocity += _gravity * _gravityMultiplier * Time.deltaTime;
-        }
-
-        _moveDir.y += _velocity;
-    }
-
     private void ApplyMovement()
     {
-        Controller.Move(_moveDir.normalized * Speed * Time.deltaTime);
+        //On ground
+        if(Grounded)
+            Rb.AddForce(_moveDir.normalized * Speed * 10f, ForceMode.Force);
+        else
+            Rb.AddForce(_moveDir.normalized * Speed * 10f * _airMultiplier, ForceMode.Force);
     }
 
-    private bool IsGrounded() { return Controller.isGrounded; }
+    private void SpeedControl(){
+        Vector3 flatVel = new Vector3(Rb.velocity.x, 0f, Rb.velocity.z);
+
+        if(flatVel.magnitude > Speed)
+        {
+            Vector3 limitedVel = flatVel.normalized * Speed; 
+            Rb.velocity = new Vector3(limitedVel.x, Rb.velocity.y, limitedVel.z);
+        }
+    }
+
 }
